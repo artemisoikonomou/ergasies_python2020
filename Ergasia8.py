0b81cd03d61515ed33421addc3779f3e2dbc8711
artemisoikonomou <59887252+artemisoikonomou@users.noreply.github.com>
2020-02-10 20:21:30 +0200

    Τελική άσκηση

--- Ergasia8.py
+++ Ergasia8.py
@@ -1,48 +1,71 @@
 import random
 
-
 # Φτιάχνω μια λίστα με 3 στοιχεία που αποτελούν τα φανάρια
 # --> κάθε στοιχείο της λίστας θα έχει 3 τιμές "name" : "fanari 1", "cars": 10, "isGreen": True
 fanaria = [ { "name":"fanari1", "cars":10, "isGreen": False}, 
             { "name":"fanari2", "cars":20, "isGreen": False},
             { "name":"fanari3", "cars":30, "isGreen": False} ]
 
-def minmaxCars():
-    minCars = fanaria[0]["cars"]
+def maxCars():
     maxCars = fanaria[0]["cars"]
     fanariMax = 0
     for i in range(1,3):
-        if fanaria[i]["cars"] < minCars:
-            minCars = fanaria[i]["cars"]
         if fanaria[i]["cars"] > maxCars:
             maxCars = fanaria[i]["cars"]
             fanariMax = i
     
-    return minCars, maxCars, fanariMax
+    return fanariMax
+
+def comeGo(isGreen):
+    come=0
+    go=0
+    if isGreen==True:
+        go = random.randrange(5, 10)
+    else:
+        come = random.randrange(0, 5)
+
+    return come,go
 
 
 def populateCars():
     for fanari in fanaria:
-        fanari["cars"] = random.randrange(1, 100)
+        fanari["cars"] = random.randrange(1, 20)
 
 def printFanaria():
+    print("FANARIA STATUS --------------------------------------------------------")
     for fanari in fanaria:
         print(f'Name: {fanari["name"]}, Cars: {fanari["cars"]}, Is Green: {fanari["isGreen"]}')
+    print("FANARIA STATUS END --------------------------------------------------------")
 
 # Ξεκινάει το πρόγραμμα
+random.seed()
 
 # Βάζω σε κάθε φανάρι τυχαίο αριθμό αυτοκινήτων (από 1 έως 100)
 populateCars()
 
 # Επανέλαβε για όσο κανένα φανάρι δεν έχει μηδέν αυτοκίνητα:
-#     Βρές ποιο είναι το φανάρι με τα περισσότερα αυτοκίνητα  
-#          --> Κάντο πράσινο
-#          --> Να φύγουν τυχαία από 5-10 αυτοκίνητα
-#          --> Να διαιρεθούν ακέραια / 2 τα αυτοκίνητα που φύγανε και να πάνε στα άλλα 2 φαναρια
-#          --> Τύπωσε για όλα τα φανάρια το όνομα τους, την κατάσταση τους και τα αυτοκίνητα που τώρα έχουν
+while fanaria[0]["cars"]!=0 and fanaria[1]["cars"] !=0 and fanaria[2]["cars"]!=0 :
+    
+    # printFanaria() --> Για να ελέγξουμε τον αλγόριθμο σε κάθε στάδιο
+    
+    #     Βρές ποιο είναι το φανάρι με τα περισσότερα αυτοκίνητα
+    fanariMax = maxCars()  
 
-minCars, maxCars, fanariMax = minmaxCars()       
-print(f"minCars {minCars}")
-print(f"maxCars {maxCars}")
-print(f"Fanari with max {fanariMax}")
-printFanaria()
+    #     --> Κάντο πράσινο
+    fanaria[fanariMax]["isGreen"] = True
+
+    #     --> Να φύγουν τυχαία από 5-10 αυτοκίνητα από το πράσινο φανάρι
+    come, go = comeGo(fanaria[fanariMax]["isGreen"])
+    print(f'Name: {fanaria[fanariMax]["name"]}, Cars: {fanaria[fanariMax]["cars"]}, Is Green: {fanaria[fanariMax]["isGreen"]}, Cars coming: {come}, Cars Leaving: {go}')
+    if fanaria[fanariMax]["cars"] - go >= 0:
+        fanaria[fanariMax]["cars"] = fanaria[fanariMax]["cars"] - go
+
+    #     --> Να μπουν τυχαία από 0-5 αυτοκίνητα στα άλλα φανάρια
+    for i in range(0,3):
+        if(i!=fanariMax):
+            come, go = comeGo(fanaria[i]["isGreen"])
+            print(f'Name: {fanaria[i]["name"]}, Cars: {fanaria[i]["cars"]}, Is Green: {fanaria[i]["isGreen"]}, Cars coming: {come}, Cars Leaving: {go}')
+            fanaria[i]["cars"] = fanaria[i]["cars"] + come
+    
+    fanaria[fanariMax]["isGreen"] = False
+
